f62f8b11a3f6684df98b399b47d5988246f85834
d.osipov <d.osipov@skillaz.ru>
2021-04-08 13:21:56 +0300

    small refactoring code structure

--- app/app.js
+++ app/app.js
@@ -2,30 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 
-const path = __dirname + '/views/';
+// const path = __dirname + '/views/';
 const port = 8080;
 
-const jwt = require('jsonwebtoken');
 
-app.use(express.static(path));
+// app.use(express.static(path));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.options('*', cors());
 
 
-app.post('/auth',(req,res)=>{
-  console.log(req.body)
-  const token = jwt.sign({
-    data: `${req.body.name},${req.body.password}`
-  },'secret');
-  res.send(token);
-})
+require('./routes')(app,{});
 
-app.post('/registration',(req,res)=>{
-  res.send(true)
-})
-
-app.listen(port, function () {
+app.listen(port, ()=> {
   console.log('Example app listening on port 8080!')
 })
---
+++ app/routes/note_routes.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (app,db)=>{
+    app.post('/auth',(req,res)=>{
+        console.log(req.body)
+        const token = jwt.sign({
+            data: `${req.body.name},${req.body.password}`
+        },'secret');
+        res.send(token);
+    })
+
+    app.post('/registration',(req,res)=>{
+        res.send(true)
+    })
+}
